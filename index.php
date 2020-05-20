e3f6480dccdbc9f4c1a2eb0f85d29f8e680e59d7
hospodynko <oleh.hospodynko@babenkocommerce.com>
2020-05-20 19:21:39 +0300

    2

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
-phpinfo();
+Class PostData
+{
+    private $name;
+    private $email;
+    private $pass;
+
+    public function __construct()
+    {
+        $this->getData();
+    }
+//
+//    public function getData()
+//    {
+//        if($_POST) {
+//            foreach ($_POST as $key => $value) {
+//
+//            }
+//            list($this->name, $this->email, $this->pass) = $_POST;
+//        }
+//        echo $this->name . 1;
+//        echo $this->email;
+//        echo $this->pass;
+//    }
+}
+
+$a = new PostData();
+var_dump($_POST);
+if ($_POST){
+    echo $_POST['email'];
+} else {
+    echo $_POST['email'] . 1;
+}
